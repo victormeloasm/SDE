bd2863a2b37d8fee2fb461b8cfd687b81bcaa418
Víctor Duarte Melo <victormeloasm@gmail.com>
2025-03-08 10:35:52 -0300

    Add files via upload

---
+++ src/shirt_drying_estimator/android/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.shirt_drying_estimator"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"  // Versão necessária do NDK
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.shirt_drying_estimator"
+        // Atualize os valores abaixo conforme necessário.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Configure sua assinatura para a build de release.
+            // Atualmente está utilizando a configuração de debug para `flutter run --release` funcionar.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+// Suprime os avisos sobre opções obsoletas do Java (source e target value 8)
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Xlint:-options")
+}
---
+++ src/shirt_drying_estimator/android/app/src/main/kotlin/com/example/shirt_drying_estimator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shirt_drying_estimator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
